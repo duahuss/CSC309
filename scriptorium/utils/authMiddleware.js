ac77f1657950d629b6dcc40d276ffdb0da20b92b
maleeha-599 <144400784+maleeha-599@users.noreply.github.com>
2024-11-03 16:06:48 -0500

    Update authMiddleware.js
    removed commented out code

--- scriptorium/utils/authMiddleware.js
+++ scriptorium/utils/authMiddleware.js
@@ -1,36 +1,3 @@
-// import { verifyToken } from './jwt';
-// import { PrismaClient } from '@prisma/client';
-
-// const prisma = new PrismaClient();
-
-// export async function authMiddleware(req, res, next) {
-//     const token = req.headers.authorization?.split(" ")[1];
-
-//     if (!token) {
-//         return res.status(401).json({ message: "Token not provided" });
-//     }
-
-//     // Check if the token is blacklisted
-//     const blacklistedToken = await prisma.blacklistedToken.findUnique({
-//         where: { token },
-//     });
-//     if (blacklistedToken) {
-//         return res.status(401).json({ message: "Token is invalid" });
-//     }
-
-//     try {
-//         const decoded = verifyToken(token);
-//         if (!decoded) {
-//             throw new Error("Invalid token");
-//         }
-//         req.userId = decoded.userId;
-//         next();
-//     } catch (error) {
-//         console.error("Authentication error:", error.message);
-//         return res.status(401).json({ message: "Token is invalid" });
-//     }
-// }
-
 import { verifyToken } from './jwt';
 import { PrismaClient } from '@prisma/client';
 
